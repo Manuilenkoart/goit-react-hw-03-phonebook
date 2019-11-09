f837a9a4885a37dfbbb3ae47c5a766203f5ca348
Manuilenkoart <manuilenkoart@gmail.com>
2019-11-09 17:48:27 +0200

    Updates

---
+++ precache-manifest.e35b790592119207c92268dde1546436.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0139d118b4801a7b7bd38be81e557958",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "2f656ea12ee24098cf73",
+    "url": "/goit-react-hw-03-phonebook/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "2f656ea12ee24098cf73",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b6660b3f.chunk.js"
+  },
+  {
+    "revision": "5745b782bbe024e14b46",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.c0643b11.chunk.js"
+  },
+  {
+    "revision": "443d983ae3bc5dfd921c",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.5b2c9567.js"
+  }
+]);
